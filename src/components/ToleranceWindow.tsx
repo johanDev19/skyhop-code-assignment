0db800484366aecc83184460d14c2b78937ccf40
Johan H. Guzman Gil <johan.dev19@gmail.com>
2023-12-07 09:40:10 -0400

    fix: add missed component to ToleranceWindow component

--- src/components/ToleranceWindow.tsx
+++ src/components/ToleranceWindow.tsx
@@ -10,30 +10,33 @@ function ToleranceWindow({ onChange }: Props) {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <div className="flex items-center">
-      <Switch
-        checked={enabled}
-        onChange={() => {
-          setEnabled(!enabled);
-          onChange(!enabled);
-        }}
-        className={`${
-          enabled ? "bg-skyhopBlue" : "bg-gray-200"
-        } relative inline-flex h-6 w-11 items-center rounded-full`}
-      >
-        <span className="sr-only">Enable notifications</span>
-        <span
+    <div className="flex flex-col items-start">
+      <Text as="h6">Tolerance Window:</Text>
+      <div className="flex mt-2">
+        <Switch
+          checked={enabled}
+          onChange={() => {
+            setEnabled(!enabled);
+            onChange(!enabled);
+          }}
           className={`${
-            enabled ? "translate-x-6" : "translate-x-1"
-          } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-        />
-      </Switch>
-      <div className="flex items-center justify-center gap-3">
-        <Text as="span" className="!text-skyhopBlue text-xs ml-2">
-          Toggle {enabled ? "ON" : "OFF"}
-        </Text>
-        <div className="flex items-center gap-3 justify-center">
-          | <ClockIcon className="w-4 h-4" /> Select Tolerance Level
+            enabled ? "bg-skyhopBlue" : "bg-gray-200"
+          } relative inline-flex h-6 w-11 items-center rounded-full`}
+        >
+          <span className="sr-only">Enable notifications</span>
+          <span
+            className={`${
+              enabled ? "translate-x-6" : "translate-x-1"
+            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+          />
+        </Switch>
+        <div className="flex items-center justify-center gap-3">
+          <Text as="span" className="!text-skyhopBlue text-xs ml-2">
+            Toggle {enabled ? "ON" : "OFF"}
+          </Text>
+          <div className="flex items-center gap-3 justify-center">
+            | <ClockIcon className="w-4 h-4" /> Select Tolerance Level
+          </div>
         </div>
       </div>
     </div>
