fc11b0d4392c5c1ec8f0849af41df09bf0d7fb51
Johan H. Guzman Gil <johan.dev19@gmail.com>
2023-12-06 23:07:37 -0400

    refactor: change the component Title for Text for make it more dynamic

---
+++ src/components/Text.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Divider from "./Divider";
+
+interface Props {
+  children: React.ReactNode;
+  as: "div" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "label";
+  underline?: boolean;
+  className?: string;
+}
+
+function Text({ children, underline, className, as }: Props) {
+  const Element = as;
+
+  return (
+    <>
+      <Element className={`text-3xl font-bold w-fit ${className}`}>
+        {children}
+        {underline && <Divider />}
+      </Element>
+    </>
+  );
+}
+
+Text.defaultProps = {
+  underline: true,
+  as: "p",
+};
+
+export default Text;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,14 @@
 import Divider from "../components/Divider";
 import Dropdown from "../components/Dropdown";
-import Title from "../components/Title";
+import Text from "../components/Text";
 
 function Home(): JSX.Element {
   return (
     <section className="container m-auto flex flex-col items-center p-10 gap-10">
       <div className="flex justify-center">
-        <Title underline>Document Upload</Title>
+        <Text as="h1" underline>
+          Document Upload
+        </Text>
       </div>
       <div className="flex gap-20 justify-around w-full">
         <div className="flex flex-col gap-2 w-full">
